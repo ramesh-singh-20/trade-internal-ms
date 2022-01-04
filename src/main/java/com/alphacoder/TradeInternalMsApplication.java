35bced583430d17905f7a154f4e47747ca7fd18b
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-01-04 11:46:05 -0500

    Initial project set up.

---
+++ src/main/java/com/alphacoder/TradeInternalMsApplication.java
@@ -0,0 +1,13 @@
+package com.alphacoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeInternalMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeInternalMsApplication.class, args);
+	}
+
+}
